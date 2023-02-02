41134a785ac8663e02438384ca3186c56bc0b2b3
DylanRhinehart <dylanrhinehart@gmail.com>
2023-02-01 17:00:46 -0800

    commit tree feb1bd7d5b0716678d6709e3e66f6b3869a37c3b

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,'Mr. Chocolate', 21, 'M', 'Brown', 'Aggressive', True);
+INSERT INTO bears VALUES (2,'Rowdy', 21, 'M', 'Brown', 'Aggressive', True);
+INSERT INTO bears VALUES (3,'Tabitha', 21, 'M', 'Brown', 'Aggressive', True);
+INSERT INTO bears VALUES (4,'Sergeant Brown', 21, 'M', 'Brown', 'Aggressive', True);
+INSERT INTO bears VALUES (5,'Melissa', 21, 'M', 'Brown', 'Aggressive', True);
+INSERT INTO bears VALUES (6,'Grinch', 21, 'M', 'Brown', 'Aggressive', True);
+INSERT INTO bears VALUES (7,'Wendy', 21, 'M', 'Brown', 'Aggressive', True);
+INSERT INTO bears (id, age, sex, color, temperament, alive) VALUES (8, 21, 'M', 'Brown', 'Aggressive', True);
